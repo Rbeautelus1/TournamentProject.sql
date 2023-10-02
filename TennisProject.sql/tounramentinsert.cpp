5c7721d7050fbd69405fdc8eb180dacb504ec7a5
Rbeautelus1 <103546272+Rbeautelus1@users.noreply.github.com>
2023-10-02 19:51:37 -0400

    Update tounramentinsert.cpp

--- TennisProject.sql/tounramentinsert.cpp
+++ TennisProject.sql/tounramentinsert.cpp
@@ -1,10 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
-/*
- Include directly the different
- headers from cppconn/ and mysql_driver.h + mysql_util.h
- (and mysql_connection.h). This will reduce your build time!
-*/
+
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
@@ -13,17 +9,6 @@
 #include <cppconn/prepared_statement.h>
 using namespace std;
 
-// This program adds a record to the musician table and 
-// a record to the instrumentsplayed table with the musicians id
-// as a foreign key. Both tables use the auto increment feature to generate
-// the primary key. 
-
-// Compile:
-// g++ -I/usr/include musicinsert.cpp -o musicinsert -I /usr/local/lib -lmysqlcppconn
-//
-// Execute:
-// ./musicinsert
-// 
 
 int main(void)
 {
